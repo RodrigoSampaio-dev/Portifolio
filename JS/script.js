330de27a93569eac9131134090d46653cc2553f9
Rodrigo Sampaio <digao_sampaio16@live.com>
2024-01-13 15:20:40 -0300

    Adicionou um Portifólio

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+document.getElementById("paragrafo").innerHTML = "Me chamo Rodrigo Sampaio, tenho 28 anos, moro atualmente no estado do Rio de Janeiro, sou estudante de Analise e desenvolvimento de sistemas, estou cursando o 3º periodo na instituição Estácio."
+
+document.getElementById("paragrafo_2").innerHTML = "Amo esportes, atualmente treino musculação e nos tempos vagos gosto de jogar um game on-line."
+
+document.getElementById("paragrafo_3").innerHTML = "Conhecimento de Linguagens"
